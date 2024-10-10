6cd2fd875ba9186b2328db9b56864022d0b51437
Bruno Luan <Brunoluan123@gmail.com>
2024-10-10 19:48:22 -0300

    soma 10 + 10

---
+++ src/testGit/Gittest.java
@@ -0,0 +1,10 @@
+package testGit;
+
+public class ;Gittest {
+    public static void main(String[] args){
+        int a  = 10;
+        int b = 10;
+        int soma = b+=a;
+        System.out.println("O valor de b + a é: "+soma);
+    }
+}
